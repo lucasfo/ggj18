e515fb527bf0537ae5e9a1c6215326924a0ab09d
Lucas Fernandes de Oliveira <lfo14@inf.ufpr.br>
2018-01-27 22:29:24 +0000

    Fix catome bars
    Signed-off-by: Lucas Fernandes de Oliveira <lfo14@inf.ufpr.br>

--- game/gui/Bar/bar.gd
+++ game/gui/Bar/bar.gd
@@ -11,11 +11,11 @@ func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	get_node("back").set_region_rect(Rect2(128,0,0, height))
-	get_node("back").set_pos(Vector2(-64,0))
+	get_node("back").set_pos(Vector2(64,0))
 	pass
 	
 func updateRage(rage):
 	var newWidth = int(rage*width/100.0)
 	get_node("back").set_region_rect(Rect2(128,0,newWidth, height))
-	get_node("back").set_pos(Vector2(-64 + newWidth/2,0))
+	get_node("back").set_pos(Vector2(64 - newWidth/2,0))
 	pass
