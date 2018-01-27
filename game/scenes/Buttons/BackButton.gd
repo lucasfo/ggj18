c05d68aae45e36555e4fc0070857eb61ee0030a2
Leandro-PC\Leandro <lbrguitar@gmail.com>
2018-01-27 02:57:59 -0200

    Botão de continuar e cena de how to play sem nada

--- game/scenes/Buttons/BackButton.gd
+++ game/scenes/Buttons/BackButton.gd
@@ -6,6 +6,7 @@ func _ready():
 
 func backButtonPressed():
 	var sceneName = get_tree().get_current_scene().get_name()
-	print(sceneName)
 	if(sceneName == "CutScene" or sceneName == "Credits"):
 		get_tree().change_scene("res://scenes/menu.tscn")
+	elif(sceneName == "HowToPlay"):
+		get_tree().change_scene("res://scenes/cutScene.tscn")
