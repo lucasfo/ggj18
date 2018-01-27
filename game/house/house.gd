90c1a05d65d681a9056c43a55e69dfd11aa8565d
Lucas Fernandes de Oliveira <lfo14@inf.ufpr.br>
2018-01-27 12:32:30 +0000

    Spawn and remove balloons
    Signed-off-by: Lucas Fernandes de Oliveira <lfo14@inf.ufpr.br>

--- game/house/house.gd
+++ game/house/house.gd
@@ -17,32 +17,43 @@ export (int) var base = 12
 export(int) var initialOffset = 0
 export(int) var offset  = 0 # space between balloons
 export(int) var bonusTime = 0 # Time  gained when a balloon is delevery
-export(int) var auraTime = 0	 # Time gained when house is cleared
 export(int) var maxBalloons = 1 # Maximum of balloons possible
 export(int) var houseId = 1
+export(int) var houseLeft = true
 
 var cooldownMin = 0
 var cooldownMax = 0
 var initialTime = 0
 var score = 0
 var cooldown = 0
+var baseBalloons
+
+signal house_end
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	active = true
+	active = false
 	adjustHardness(hardness)
 	calcCooldown()
 	set_process(true)
 	balloons = []
+	timer = initialTime* 1000
 	pass
 
 func _process(delta):
-	if cooldown > OS.get_ticks_msec():
+	var ticks =  OS.get_ticks_msec()
+	print (ticks, " ", cooldown, " ", timer)
+	if cooldown < ticks && not active:
+		active = true
 		spawnBalloon()
-		set_process(false)
+	if active && timer < ticks:
+		active = false
+		sendAura()
+		
 
 func spawnBalloon():
+	print("SPaw balao")
 	active = true
 	var nBalloons = (randi()%(maxBalloons )) +1
 	var balloonType = randi()%2
@@ -52,21 +63,31 @@ func spawnBalloon():
 		balloon.setType(balloonType)
 		add_child(balloon)
 		balloon.set_pos(Vector2(0, initialOffset))
-	print("Spawnei - ", nBalloons)		
+	baseBalloons = nBalloons
+	timer = OS.get_ticks_msec() + initialTime*1000		
 	pass
 
-func receiveAura():
+func receiveAura(auraTime):
 	if active:
 		timer += auraTime
 	pass
 
 func sendAura():
-	set_process(true)
+	print("Morre balao")
 	calcCooldown()
+	for balloon in balloons:
+		remove_child(balloon)
+		balloon.queue_free()
+	var score = (baseBalloons * hardness) + int(timer)
+	var rage = balloons.size() * 5
+	emit_signal("house_end", houseId, houseLeft, score, rage, int(timer), balloons.size())
 	# send aura to neighboors
 	pass
 
 func removeBalloon():
+	var balloon = balloons.front()
+	remove_child(balloon)
+	balloon.queue_free()
 	balloons.pop_front()
 	timer += bonusTime
 	if balloons.size() == 0:
@@ -90,9 +111,11 @@ func adjustHardness(hard):
 	initialTime = houseId*base - hardness
 	cooldownMin = initialTime - hardness
 	cooldownMax = int(cooldownMin * 1.5)
+	print (initialTime, " ", cooldownMin, " ", cooldownMax)
 
 func calcCooldown():
 	cooldown = (randi()%(cooldownMax - cooldownMin)) + cooldownMin
+	cooldown *= 1000
 	cooldown += OS.get_ticks_msec()
 	
 
--- game/scenes/game/game.gd
+++ game/scenes/game/game.gd
@@ -14,6 +14,9 @@ var lastSpawn
 var leftHouses
 var rightHouses
 
+var score = 0
+var rage = 0
+
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
@@ -24,4 +27,37 @@ func _ready():
 		leftHouses.append(get_node(path))
 	for path in rightHousesPath:
 		rightHouses.append(get_node(path))
+	
+	var thisNode = get_node(".")
+	thisNode.connect("house_end", self, "houseHandler")
+
+	score = 0
+	rage = 0
+	pass
+
+func houseHandler(houseId, houseLeft, houseScore, houseRage, time, remainingBalloons):
+	if remainingBalloons > 0:
+		rage += houseRage
+		if rage >= 100:
+			endGame()
+	else:
+		score += houseScore
+		var houses
+		if houseLeft:
+			houses = leftHouses
+		else:
+			houses = rightHouses
+
+		if houseId == 1:
+			houses[2].receiveAura(time)
+		elif houseId == 5:
+			houses[4].receiveAura(time)
+		else:
+			houses[houseId -1].receiveAura(time)
+			houses[houseId +1].receiveAura(time)
+			
+	pass
+
+func endGame():
+	print("endGame")
 	pass
